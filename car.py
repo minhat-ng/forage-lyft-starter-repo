108a037e81cd15eb9b70027d0127e5da1240ecff
doristrungvuong@gmail.com <doristrungvuong@gmail.com>
2024-01-19 18:26:16 -0500

    added tires_condition array into cars init function

--- car.py
+++ car.py
@@ -1,10 +1,11 @@
 from serviceable import Serviceable
 
 class Car(Serviceable): 
-    def __init__(self, engine_type, battery_type):
+    def __init__(self, engine_type, battery_type, tires):
         self.engine = engine_type
         self.battery = battery_type
+        self.tires = tires
     
     def needs_service(self) -> bool:
-        return self.engine.needs_service() or self.battery.needs_service() 
+        return self.engine.needs_service() or self.battery.needs_service() or self.tires.needs_service()
     
